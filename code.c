a07a39bfc5d99324678fb2b9d9ffe0f52b41101b
Coldreams88 <2497842026@qq.com>
2024-06-06 19:38:06 +0800

    change upload

--- code.c
+++ code.c
@@ -3,7 +3,19 @@
 int main()
 {
 
-    printf("test code\n");
+
+
+
+
+
+	
+    printf("code1\n");
+    
+    printf("code2\n");
+    printf("code3\n");
+    printf("code4\n");
+    printf("code5\n");
+    printf("code6\n");
     return 0;
 
 }
